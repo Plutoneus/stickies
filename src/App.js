d25e18404e2f56d88cc0f9219603138e3e16d7cb
Pluto <qwtrplx@gmail.com>
2020-12-09 21:49:57 -0600

    Init

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+    return (<div className="App">
+        <header className="App-header">
+            <h1>Stickies.</h1>
+        </header>
+    </div>);
+}
+
+export default App;
